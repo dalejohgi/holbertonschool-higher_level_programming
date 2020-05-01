02dfb08b22a0fdbae5035073655368bc9cb828ff
David Hincapie <1508@holbertonschool.com>
2020-05-01 00:40:22 +0000

    Infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = 1
+    result = 0
+    for i in range(1, len(sys.argv)):
+        number = int(sys.argv[i])
+        result += number
+    print(result)
