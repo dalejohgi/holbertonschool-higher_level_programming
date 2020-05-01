7009e27af59cc9ae7fcc34176c91fec76def6280
David Hincapie <1508@holbertonschool.com>
2020-05-01 00:28:23 +0000

    Python command line args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = 1
+    if (len(sys.argv) == 1):
+        print("0 arguments.")
+    elif (len(sys.argv) == 2):
+        print("{:d} argument:".format(i))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+    if (len(sys.argv) != 1):
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
