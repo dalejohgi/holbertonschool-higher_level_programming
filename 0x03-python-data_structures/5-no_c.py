1c27fb30cbb07233e8b881fdfe3d826cd4b040da
David Hincapie <1508@holbertonschool.com>
2020-05-05 20:18:01 +0000

    No C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if my_string is None:
+        return
+    new_string = ''
+    for i in range(len(my_string)):
+        if my_string[i] is 'c' or my_string[i] is 'C':
+            continue
+        else:
+            new_string += my_string[i]
+    return new_string
