c69dcddd1e9a7eff4013a1a3f61f12d93a6b4d3f
David Hincapie <1508@holbertonschool.com>
2020-05-06 02:08:37 +0000

    Try none cases

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if len(my_list) is 0:
-        return None
+        return
     elif idx > len(my_list) - 1 or idx < 0:
         return my_list
     else:
