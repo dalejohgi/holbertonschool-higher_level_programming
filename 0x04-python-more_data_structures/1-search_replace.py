775ff4032b8831df9fe3480f42ebf534adf08277
David Hincapie <1508@holbertonschool.com>
2020-05-08 02:57:50 +0000

    Replace an element

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    def searcher(element):
+        return (element if element is not search else replace)
+    return [list(map(searcher, my_list))]
