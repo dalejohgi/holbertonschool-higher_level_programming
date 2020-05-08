1f21585f764eb1f95ef654ee55f9165f480ea891
David Hincapie <1508@holbertonschool.com>
2020-05-08 03:00:54 +0000

    resolve square brackets

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 def search_replace(my_list, search, replace):
     def searcher(element):
         return (element if element is not search else replace)
-    return [list(map(searcher, my_list))]
+    return list(map(searcher, my_list))
