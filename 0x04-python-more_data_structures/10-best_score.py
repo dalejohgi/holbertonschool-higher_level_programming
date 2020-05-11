00f3c0b6f977e7ed1f699d0aa3e0b4436d2a67b9
David Hincapie <1508@holbertonschool.com>
2020-05-11 03:30:12 +0000

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_one = 0
+    best_key = ""
+    for name, score in a_dictionary.items():
+        if score >= best_one:
+            best_one = score
+            best_key = name
+    return (best_key)
