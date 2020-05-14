ca26a06de780a72901605ee8a228747b924d2b35
David Hincapie <1508@holbertonschool.com>
2020-05-14 00:10:05 +0000

    Fix none cases

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if not a_dictionary:
-        return None
     keys = []
     for key, val in a_dictionary.items():
         if val is value:
