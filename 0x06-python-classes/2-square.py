b71d607ffbf1316b3f076c8907ccbbe6d579d74a
David Hincapie <1508@holbertonschool.com>
2020-05-20 00:25:03 +0000

    Handling exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Empty class"""
+
+
+class Square:
+    """Square class wit a private attribute"""
+    def __init__(self, size=0):
+        """Initializes with a attribute size"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
