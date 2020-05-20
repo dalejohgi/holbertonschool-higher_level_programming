b16c56141f509f4bbe2a4e0da2952e190953621a
David Hincapie <1508@holbertonschool.com>
2020-05-20 00:32:36 +0000

    Returns the area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Empty class"""
+
+
+class Square:
+    """Square class wit a private attribute"""
+    def __init__(self, size=0):
+        """Initializes with a attribute size"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+
+    def area(self):
+        """Returns the area of a square"""
+        return (int(self.__size) * int(self.__size))
