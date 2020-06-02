766c4e4f8daf2ac60e9bbdc7084adac061b9c393
David Hincapie <1508@holbertonschool.com>
2020-06-02 23:53:36 +0000

    Try initializating

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,10 @@
 
 class MyList(list):
     """Class"""
+    def __init__(self):
+        """Initializer"""
+        super().__init__()
+
     def print_sorted(self):
         """Function"""
         print(sorted(self))
