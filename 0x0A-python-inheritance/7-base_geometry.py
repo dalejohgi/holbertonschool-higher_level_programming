371db20ea7942be8759cf0a33792ac458abfed8a
David Hincapie <1508@holbertonschool.com>
2020-06-03 00:56:29 +0000

    Add integer validator function

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,6 +12,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Integer validator function"""
         if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
+            raise TypeError('{:s} must be an integer'.format(name))
         elif value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+            raise ValueError('{:s} must be greater than 0'.format(name))
