948e528f709bf9873cf950c4c0216c4c1ec3f9b1
David Hincapie <1508@holbertonschool.com>
2020-06-03 00:39:12 +0000

    Inherits from a class but nit the class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""Module 4-inherits_from"""
+
+
 def inherits_from(obj, a_class):
+    """[Checks if an object inherits from a class]
+
+    Arguments:
+        obj {Object}
+        a_class {Class}
+    """
+    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
