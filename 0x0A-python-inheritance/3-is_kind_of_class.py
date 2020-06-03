f9b1473c2aee1c6440fd58ec13923a62ef1c8c2f
David Hincapie <1508@holbertonschool.com>
2020-06-03 00:25:08 +0000

    Is kind of class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""Module 3-is_kind_off_class"""
+
+
 def is_kind_of_class(obj, a_class):
+    """[Checks if an object is an instance, or inherits from a class]
+
+    Arguments:
+        obj {Object}
+        a_class {Class}
+    """
+    return (isinstance(obj, a_class))
