25eae835db64357b16604be5f400dda48a152d71
David Hincapie <1508@holbertonschool.com>
2020-06-03 00:06:29 +0000

    Is the same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
+    """[Evaluate classes]
+
+    Arguments:
+        obj {[Object]}
+        a_class {[Class]}
+
+    Returns:
+        [Boolean] -- [True if the object belongs to the class, else False]
+    """
+    if type(obj) == a_class:
+        return (True)
+    else:
+        return (False)
