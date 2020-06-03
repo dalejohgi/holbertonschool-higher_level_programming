97ab1c12cbcf674b2d5f465c80cda4bd77c2dab0
David Hincapie <1508@holbertonschool.com>
2020-06-03 23:30:05 +0000

    Add end=

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -11,7 +11,7 @@ def read_lines(filename="", nb_lines=0):
             line_counter += 1
         my_file.seek(0)
         if nb_lines <= 0 or nb_lines >= line_counter:
-            print(my_file.read())
+            print(my_file.read(), end="")
         else:
             for i in range(nb_lines):
                 print(my_file.readline(), end="")
