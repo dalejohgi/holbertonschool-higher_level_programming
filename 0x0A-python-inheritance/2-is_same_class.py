8229c957781863e95d0eb3b29d026827efcb6051
David Hincapie <1508@holbertonschool.com>
2020-06-03 00:11:38 +0000

    Delete conditionals

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,7 +9,4 @@ def is_same_class(obj, a_class):
     Returns:
         [Boolean] -- [True if the object belongs to the class, else False]
     """
-    if type(obj) == a_class:
-        return (True)
-    else:
-        return (False)
+    return (type(obj) == a_class)
