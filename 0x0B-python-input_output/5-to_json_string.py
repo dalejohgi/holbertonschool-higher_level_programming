c5b375cbde21204a5da62d36397364c40ca9043d
David Hincapie <1508@holbertonschool.com>
2020-06-04 00:46:38 +0000

    Add task 5

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""Module 5-to_json_string"""
+
+
+import json
+
+
 def to_json_string(my_obj):
+    """Converts an string to a JSON object representation"""
+    j_obj = json.dumps(my_obj)
+    return j_obj
