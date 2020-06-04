241e94bd83564dd3a00ce46890d202806200769a
David Hincapie <1508@holbertonschool.com>
2020-06-04 02:27:51 +0000

    Add item json

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+"""Module 9-add_item"""
+
+
+from sys import argv
+import json
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+
+filename = "add_item.json"
+try:
+    python_list = load_from_json_file(filename)
+except FileNotFoundError:
+    python_list = []
+
+for argument in argv[1:]:
+    python_list.append(argument)
+save_to_json_file(python_list, filename)
