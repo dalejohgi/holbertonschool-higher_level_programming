c850afac933b0e01d99909d5b49fd8bd4ce519ca
David Hincapie <1508@holbertonschool.com>
2020-06-04 00:51:59 +0000

    Add json converter to python obj

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""Moldule 6-from_json_string"""
+
+
+import json
+
+
 def from_json_string(my_str):
+    """Converts json object into a Python object"""
+    py_obj = json.loads(my_str)
+    return py_obj
