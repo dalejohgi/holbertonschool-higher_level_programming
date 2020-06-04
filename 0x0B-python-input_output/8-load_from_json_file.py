2feddab9bdbf86bb6b56ace96a8065bdade5221a
David Hincapie <1508@holbertonschool.com>
2020-06-04 02:00:09 +0000

    Load from jsno

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""Module 8-load_from_json_file"""
+
+
+import json
+
+
 def load_from_json_file(filename):
+    """creates an Object from a JSON file"""
+    with open(filename, "r", encoding='utf-8') as json_file:
+        py_obj = json.load(json_file)
+    return py_obj
