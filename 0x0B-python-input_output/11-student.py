d7e9538c182184730d3397a00d135fe8cf4616d2
David Hincapie <1508@holbertonschool.com>
2020-06-05 03:29:30 +0000

    Student class

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Module 11-student.py"""
+
+
+class Student:
+    """Class that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializator for student class"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dict representation of a student instance"""
+
+        return self.__dict__
