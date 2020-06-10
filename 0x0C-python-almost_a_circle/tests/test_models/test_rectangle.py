77fbd8e253622b8e7bac978f58fe9c4f631ef904
David Hincapie <1508@holbertonschool.com>
2020-06-10 16:44:11 +0000

    Fix pep8

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -12,13 +12,6 @@ Base = base.Base
 class TestHbRequirements(unittest.TestCase):
     """Tests for Holberton Requirements"""
 
-    def test_pep8_conformance_base(self):
-        """Test that models/base.py conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
     def test_mod_doc(self):
         """Checks the module documentation"""
         self.assertGreater(len(base.__doc__), 0)
