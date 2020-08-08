1d3a4ec9f235e47bfc47948a84272cdf8e2183b9
David <1508@holbertonschool.com>
2020-08-08 16:57:30 +0000

    Add first 5 tasks

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""[takes in the name of a state as an argument and lists
+    all cities of that state, using the database hbtn_0e_4_usa]
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities INNER JOIN states\
+                 ON state_id = states.id WHERE states.name = %s ORDER BY\
+                 cities.id ASC;", (argv[4],))
+    rows = cur.fetchall()
+    separator = ', '
+    print(separator.join(row[0] for row in rows))
+    cur.close()
+    db.close()
