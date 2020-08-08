318dc3082307634f92df764daf34b346601c8a45
David <1508@holbertonschool.com>
2020-08-08 17:04:00 +0000

    Add first 5 tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""[Script that lists all states from the database hbtn_0e_0_usa]"""
+"""
+[Script that lists all states from the database hbtn_0e_0_usa]
+"""
 
 
 import MySQLdb
 from sys import argv
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
