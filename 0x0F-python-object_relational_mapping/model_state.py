28968f5ac964c77c2654ccd3f1fa3278acb3c66f
David <1508@holbertonschool.com>
2020-08-09 20:20:22 +0000

    Fix pep8 style

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,10 +8,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Docstring goes here"""
     __tablename__ = 'states'
     id = Column(Integer, unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
