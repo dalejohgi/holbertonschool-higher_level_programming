448f9de5d893976724dd014aa8c765f632551b1b
David <1508@holbertonschool.com>
2020-08-19 00:13:56 +0000

    Move the @ outside

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -Xs POST -H "Content-Type: application/json" -d @"$2" "$1"
+# Sends a Json POST
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
