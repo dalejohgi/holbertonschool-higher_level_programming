8b711d9f9e24cc76498e40db7a02fc698cfd030f
David <1508@holbertonschool.com>
2020-08-24 22:14:15 +0000

    Add with statement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,10 @@
 if __name__ == "__main__":
     import urllib.request as request
     holb_url = "https://intranet.hbtn.io/status"
-    response = request.urlopen(holb_url)
-    content = response.read()
-    utf8_content = content.decode('utf-8')
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(utf8_content))
+    with request.urlopen(holb_url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(utf8_content))
