53ae4a3c2d87e0606006791aea5ba8272799bf5d
David <1508@holbertonschool.com>
2020-09-21 17:30:34 -0500

    Add task 0 - 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*
+Empty class Rectangle that defines a rectangle
+*/
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  // Prints the rectangle using the character X
+
+  print () {
+    let vtcal;
+    for (vtcal = 0; vtcal < this.height; vtcal++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
