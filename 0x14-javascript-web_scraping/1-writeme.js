4def3e37c0af3662bff27658ac6ae5a60868b32e
David <1508@holbertonschool.com>
2020-09-22 17:15:45 -0500

    Fix argv format

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ Writes the content of a file.
 const fs = require('fs');
 // Rewriting the arv sintax by cutting the first 2 positions
 const argv = process.argv.slice(2);
-fs.writeFile(argv[2], argv[3], 'utf8', function (err) {
+fs.writeFile(argv[0], argv[1], 'utf8', function (err) {
   if (err) {
     return console.log(err);
   }
